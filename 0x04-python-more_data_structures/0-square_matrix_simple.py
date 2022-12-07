b0fdf6fd30c1f5bc8e8b40eb1dcdecbb0937b0d3
Chinwendu Elu <eluchinwe.com>
2022-12-07 09:49:25 -0800

    indentation fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,4 @@ def square_matrix_simple(matrix=[]):
     
     for i in range(len(matrix)):
         matrix_cpy[i] = list(map(lambda x: x**2, matrix[i]))
-
-     return (matrix_cpy)
+    return (matrix_cpy)
