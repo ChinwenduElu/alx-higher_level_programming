8e3beb233efd4a179da08c25c2b43160fd48e800
Chinwendu Elu <eluchinwe.com>
2023-01-03 11:53:14 -0800

    initial commiy

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+
+This is a module that containts a clas that avoids
+dynmaically created attributes
+
+"""
+
+class LockedClass():
+    """
+    prevent user from creating new instance attribute dynamically
+    unless attribute is "first_name"
+
+    >>> a = LockedClass()
+    >>> a.first_name = 'Melissa'
+    >>> a.first_name
+    'Melissa'
+    >>> a.last_name = 'Ng'
+
+    Traceback (most recent call last):
+    ...
+    AttributeError: 'LockedClass' object has no attribute 'last_name'
+    """
+    __slots__ = "first_name"
