e2e00d950644ed06f90d7cb80a3fb9466ba53f74
Chinwendu Elu <eluchinwe.com>
2023-01-10 10:10:45 -0800

    initial commit

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module 3-to_json_string
+
+Contains function that returns JSON representation of obj (string
+"""
+
+
+def from_json_string(my_str):
+    """Returns python data structure from JSON string
+
+    Args:
+        my_str: json string representation
+
+    Return:
+        python object
+    """
+    import json
+
+    return json.dumps(my_obj)
